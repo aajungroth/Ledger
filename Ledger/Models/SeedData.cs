72180ed306d0294d8694c0e559de10ee34a75b88
aajungroth <aajungroth@ucdavis.edu>
2018-01-01 15:55:37 -0800

    Created some seed data

--- Ledger/Models/SeedData.cs
+++ Ledger/Models/SeedData.cs
@@ -18,24 +18,28 @@ namespace Ledger.Models
                     context.Author.AddRange(
                          new AuthorClass
                          {
+                             ID = 1,
                              FirstName = "Stephen",
                              LastName = "King"
                          },
 
                          new AuthorClass
                          {
+                             ID = 2,
                              FirstName = "Joanne",
                              LastName = "Rowling"
                          },
 
                          new AuthorClass
                          {
+                             ID = 3, 
                              FirstName = "Ernest",
                              LastName = "Hemingway"
                          },
 
                          new AuthorClass
                          {
+                             ID = 4, 
                              FirstName = "Mark",
                              LastName = "Twain"
                          }
@@ -49,34 +53,38 @@ namespace Ledger.Models
                     context.Book.AddRange(
                          new BookClass
                          {
+                             ID = 1,
                              AuthorID = 1,
-                             Title = "test",
-                             ISBN = "1",
-                             PublishDate = new DateTime(2008, 3, 15)
+                             Title = "The Shining",
+                             ISBN = "978-03-85121-67-5",
+                             PublishDate = new DateTime(1977, 1, 28)
                          },
 
                          new BookClass
                          {
-                             AuthorID = 1,
-                             Title = "test",
-                             ISBN = "1",
-                             PublishDate = new DateTime(2008, 3, 15)
+                             ID = 2,
+                             AuthorID = 2,
+                             Title = "Harry Potter and the Sorcerer's Stone",
+                             ISBN = "978-04-39708-18-0",
+                             PublishDate = new DateTime(1998, 9, 1)
                          },
 
                          new BookClass
                          {
-                             AuthorID = 1,
-                             Title = "test",
-                             ISBN = "1",
-                             PublishDate = new DateTime(2008, 3, 15),
+                             ID = 3,
+                             AuthorID = 3,
+                             Title = "For Whom the Bell Tolls",
+                             ISBN = "978-06-84803-35-7",
+                             PublishDate = new DateTime(1940, 10, 21),
                          },
 
                          new BookClass
                          {
-                             AuthorID = 1,
-                             Title = "test",
-                             ISBN = "1",
-                             PublishDate = new DateTime(2008, 3, 15)
+                             ID = 4,
+                             AuthorID = 4,
+                             Title = "Adventures of Huckleberry Finn",
+                             ISBN = "978-15-03214-95-8",
+                             PublishDate = new DateTime(1884, 12, 10)
                          }
                     );
                     context.SaveChanges();
@@ -88,6 +96,7 @@ namespace Ledger.Models
                     context.Review.AddRange(
                          new ReviewClass
                          {
+                            ID = 1,
                             BookID = 1,
                             ReviewerName = "Test",
                             Body = "Test"
@@ -95,21 +104,24 @@ namespace Ledger.Models
 
                          new ReviewClass
                          {
-                            BookID = 1,
+                            ID = 2,
+                            BookID = 2,
                             ReviewerName = "Test",
                             Body = "Test"
                          },
 
                          new ReviewClass
                          {
-                            BookID = 1,
+                            ID = 3,
+                            BookID = 3,
                             ReviewerName = "Test",
                             Body = "Test"
                          },
 
                          new ReviewClass
                          {
-                            BookID = 1,
+                            ID = 4,
+                            BookID = 4,
                             ReviewerName = "Test",
                             Body = "Test"
                          }
